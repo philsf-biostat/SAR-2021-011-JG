bcde434cf6c09facce35ca3584099f05b387f0ba
Felipe Figueiredo <philsf79@gmail.com>
2021-10-22 20:00:05 -0300

    describe copiado de SAR-2021-012

--- scripts/describe.R
+++ scripts/describe.R
@@ -21,12 +21,24 @@ theme_gtsummary_language(language = "pt") # traduzir
 # minimum detectable effect size
 # interpret_d(0.5)
 
+analytical %>%
+  group_by(evangelico) %>%
+  summarise(cv = sd(num_votos, na.rm = TRUE)/mean(num_votos, na.rm = TRUE)*100)
+
+analytical %>%
+  group_by(evangelico) %>%
+  transmute(total_receita*1000000) %>%
+  skimr::skim()
+
+analytical %>%
+  filter(num_votos > 5)
 
 # tables ------------------------------------------------------------------
 
 tab_desc <- analytical %>%
   # select
   select(-id, -partido, -uf) %>%
+  select(total_receita, everything()) %>%
   tbl_summary(
     by = evangelico,
   ) %>%
@@ -35,3 +47,11 @@ tab_desc <- analytical %>%
   bold_labels() %>%
   modify_table_styling(columns = "label", align = "c")
 
+data.raw %>%
+  select(evangelico, igreja) %>%
+  tbl_summary(missing = "no")
+
+analytical %>%
+  filter(evangelico == "Evangélico") %>%
+  mutate(partido = fct_infreq(partido)) %>%
+  tbl_summary(include = partido)
