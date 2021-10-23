90aae3e744cf10b6aa51ff1dbb1ba0c81a39bf8f
Felipe Figueiredo <philsf79@gmail.com>
2021-10-22 21:11:57 -0300

    não teremos tab_inf

--- scripts/inference.R
+++ scripts/inference.R
@@ -7,25 +7,25 @@
 # tables ------------------------------------------------------------------
 
 # template p-value table
-tab_inf <- analytical %>%
-  # select
-  select(-id, -uf, -partido) %>%
-  tbl_summary(
-    by = evangelico
-  ) %>%
-  # include study N
-  add_overall() %>%
-  # pretty format categorical variables
-  bold_labels() %>%
-  # bring home the bacon!
-  add_p(
-    # use Fisher test (defaults to chi-square)
-    test = all_categorical() ~ "fisher.test",
-    # use 3 digits in pvalue
-    pvalue_fun = function(x) style_pvalue(x, digits = 3)
-  ) %>%
-  # bold significant p values
-  bold_p()
+# tab_inf <- analytical %>%
+#   # select
+#   select(-id, -uf, -partido) %>%
+#   tbl_summary(
+#     by = evangelico
+#   ) %>%
+#   # include study N
+#   add_overall() %>%
+#   # pretty format categorical variables
+#   bold_labels() %>%
+#   # bring home the bacon!
+#   add_p(
+#     # use Fisher test (defaults to chi-square)
+#     test = all_categorical() ~ "fisher.test",
+#     # use 3 digits in pvalue
+#     pvalue_fun = function(x) style_pvalue(x, digits = 3)
+#   ) %>%
+#   # bold significant p values
+#   bold_p()
 
 # Template Cohen's D table (obs: does NOT compute p)
 # tab_inf <- analytical %>%
